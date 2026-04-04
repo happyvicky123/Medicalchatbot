04ec0d8125a7500905709b4b4145b67f0c27e0f6
Vignesh <vignesh@chatbot.com>
2026-04-04 08:47:32 +0000

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Vignesh",
+    author_email="vignesh@example.com",
+    packages=find_packages(),
+    install_requires=[],
+)
