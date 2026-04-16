2ddd7ffd7b2ba49b1dac7d58605307b9be5927a1
Sridharshini_L <srid.71772118146@gct.ac.in>
2026-04-16 11:36:03 +0530

    removing debug mode

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def chat():
 #     app.run(host="127.0.0.1", port=8888, debug=True, use_reloader=False)
  
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080)
